4202a845ebc85876aff20c564652659a58365e56
pedro Torres <ped.torres@duocuc.cl>
2023-11-20 22:45:02 -0300

    comit inicial

---
+++ src/conexionBd/Conexion.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionBd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pedro
+ */
+public class Conexion {
+
+    public Conexion() {
+    }
+    
+    
+    
+     public void conexionBD() {
+        String url = "jdbc:mysql://localhost:3306/libreria";
+        String usuario = "root";
+        String contraseña = "06Villegas#ñTorres2004";
+        try {
+
+            Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
+
+            if (conexion != null) {
+
+                System.out.println("CONEXIÓN EXITOSA.");
+
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+    }
+}
---
+++ src/inventarioduoc/InventarioDuoc.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package inventarioduoc;
+
+import conexionBd.Conexion;
+
+/**
+ *
+ * @author pedro
+ */
+public class InventarioDuoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conexion con=new Conexion();
+    }
+    
+}
