8f15b20bb48d9001bd5a385b1e61e8a5456392f3
pedro Torres <ped.torres@duocuc.cl>
2023-11-24 18:35:56 -0300

    comit conexion

---
+++ src/conexionBaseDeDatos/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionBaseDeDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pedro
+ */
+public class Conexion {
+    
+     public Connection conexionBD() {
+        String url = "jdbc:mysql://localhost:3306/libreria";
+        String usuario = "root";
+        String contraseña = "06Villegas#ñTorres2004";
+        Connection conexion = null;
+        try {
+
+            conexion = DriverManager.getConnection(url, usuario, contraseña);
+
+            if (conexion != null) {
+
+                System.out.println("CONEXIÓN EXITOSA.");
+
+            }
+
+        } catch (SQLException e) {
+
+            System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+        }
+        return conexion;
+    
+    }
+}
